10127620f68afb6d0016f9ec5af5269c3950ae45
Ankur Saxena <saxank@wharton.upenn.edu>
2018-08-03 11:45:18 -0400

    Add adjustable conformity direction

--- sketch.js
+++ sketch.js
@@ -4,8 +4,9 @@ state = {
 	percentConform: parseInt(document.getElementById('percentConform').value),
 	canvasX: 200,
 	canvasY: 200,
-	maxLife: 60,
+	maxLife: 30,
 	speed: parseInt(document.getElementById('speed').value),
+	direction: parseInt(document.getElementById('direction').value),
 	dotSize: 7,
 }
 
@@ -14,10 +15,12 @@ function setup() {
 	// noSmooth();
 	renderDots();
 	updateSpeed();
+	updateDirection();
 	// Event Handlers
 	document.getElementById('ndots').onchange = renderDots;
 	document.getElementById('percentConform').onchange = moveDots;
 	document.getElementById('speed').onchange = updateSpeed;
+	document.getElementById('direction').onchange = updateDirection;
 }
 
 function draw() {
@@ -32,7 +35,14 @@ function renderDots() {
 		x = ceil(random(state.canvasX));
 		y = ceil(random(state.canvasY));
 		ellipse(x, y, state.dotSize).fill(51);
-		state.ellipse.push({x: x, y: y, life: ceil(random(state.maxLife))});
+		direction = generateDirection()
+		state.ellipse.push({
+			x: x,
+			y: y,
+			life: ceil(random(state.maxLife)),
+			directionX: direction[0],
+			directionY: direction[1],
+		});
 	}
 }
 
@@ -41,21 +51,26 @@ function moveDots() {
 	state.percentConform = parseInt(document.getElementById('percentConform').value);
 	document.getElementById('percentConfirmValue').innerHTML = state.percentConform;
 	numDotsMove = ceil((state.percentConform/100) * state.ndots);
+
+	// Move conforming dots
 	for (var i = 0; i <= numDotsMove; i++) {
-		state.ellipse[i].x = state.ellipse[i].x + state.speed;
+		state.ellipse[i].x = state.ellipse[i].x + (state.speed*generateDirection(state.direction)[0]);
+		state.ellipse[i].y = state.ellipse[i].y + (state.speed*generateDirection(state.direction)[1]);
 		state.ellipse[i].life -= 1;
 		if (state.ellipse[i].life < 0) { respawn(i); }
 		ellipse(state.ellipse[i].x, state.ellipse[i].y, state.dotSize).fill(51);
 
-		if (state.ellipse[i].x > state.canvasX) { // bring dot back on screen
-			state.ellipse[i].x = 0;
+		if (isOutOfCanvas(i)) { // bring dot back on screen
+			respawn(i);
 		}
 	}
+
+	// Move nonconforming dots
 	for (var i = numDotsMove; i <= state.ndots; i++){
 		state.ellipse[i].life -= 1;
 		if (state.ellipse[i].life < 0) { respawn(i); }
-		state.ellipse[i].x = state.ellipse[i].x + random(-1, 1)*state.speed;
-		state.ellipse[i].y = state.ellipse[i].y + random(-1, 1)*state.speed;
+		state.ellipse[i].x = state.ellipse[i].x + state.ellipse[i].directionX*state.speed;
+		state.ellipse[i].y = state.ellipse[i].y + state.ellipse[i].directionY*state.speed;
 		ellipse(state.ellipse[i].x, state.ellipse[i].y, state.dotSize).fill(51);
 	}
 	
@@ -73,3 +88,32 @@ function updateSpeed () {
 	state.speed = parseInt(document.getElementById('speed').value);
 	document.getElementById('speedValue').innerHTML = state.speed;
 };
+
+function updateDirection () {
+	state.direction = parseInt(document.getElementById('direction').value);
+	document.getElementById('directionValue').innerHTML = state.direction;
+}
+
+
+// ********** Helper functions **********
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+// Takes direction in degrees and returns a direction array as [x, y]
+function generateDirection(direction) {
+	if (!direction && direction !== 0) {
+		direction = Math.random() * 2 * Math.PI;
+	} else {
+		direction = toRadians(direction);
+	}
+	return [Math.cos(direction), Math.sin(direction)];
+}
+
+function isOutOfCanvas(dotIndex) {
+	return 	(state.ellipse[dotIndex].x > state.canvasX ||
+			state.ellipse[dotIndex].x < 0 ||
+			state.ellipse[dotIndex].y > state.canvasY ||
+			state.ellipse[dotIndex].y < 0);
+}
